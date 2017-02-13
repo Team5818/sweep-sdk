10b85f768b8fd7fb241a28e4e9616b86b98a8d18
dcyoung <dcyoung3@illinois.edu>
2017-02-13 12:06:11 -0800

    Reworked windows serial write and read

--- libsweep/src/arch/win/serial_win.cc
+++ libsweep/src/arch/win/serial_win.cc
@@ -16,9 +16,10 @@ typedef struct error {
 } error;
 
 typedef struct device {
-  HANDLE fd;
-  OVERLAPPED ro_;
-  OVERLAPPED wo_;
+  HANDLE h_comm;
+  OVERLAPPED os_reader;
+  BOOL waiting_on_read;      // Used to prevent creation of new read operation if one is outstanding
+  DWORD read_timeout_millis; // timeout interval for entire read operation
 } device;
 
 // Constructor hidden from users
@@ -84,15 +85,15 @@ device_s device_construct(const char* port, int32_t bitrate, error_s* error) {
   port_name += std::to_string(port_num);
 
   // try to open the port
-  auto hComm = CreateFile(port_name.c_str(),            // port name
-                          GENERIC_READ | GENERIC_WRITE, // read/write
-                          0,                            // No Sharing (serial ports can't be shared)
-                          NULL,                         // No Security
-                          OPEN_EXISTING,                // Open existing port only
-                          0,                            // Non Overlapped I/O
-                          NULL);                        // Null for serial ports
+  auto h_comm = CreateFile(port_name.c_str(),            // port name
+                           GENERIC_READ | GENERIC_WRITE, // read/write
+                           0,                            // No Sharing (serial ports can't be shared)
+                           NULL,                         // No Security
+                           OPEN_EXISTING,                // Open existing port only
+                           0,                            // Non Overlapped I/O
+                           NULL);                        // Null for serial ports
 
-  if (hComm == INVALID_HANDLE_VALUE) {
+  if (h_comm == INVALID_HANDLE_VALUE) {
     *error = error_construct("opening serial port failed");
     return nullptr;
   }
@@ -100,9 +101,9 @@ device_s device_construct(const char* port, int32_t bitrate, error_s* error) {
   // retrieve the current comm state
   DCB dcb_serial_params;
   dcb_serial_params.DCBlength = sizeof(dcb_serial_params);
-  if (!GetCommState(hComm, &dcb_serial_params)) {
+  if (!GetCommState(h_comm, &dcb_serial_params)) {
     *error = error_construct("retrieving current serial port state failed");
-    CloseHandle(hComm);
+    CloseHandle(h_comm);
     return nullptr;
   }
 
@@ -114,17 +115,17 @@ device_s device_construct(const char* port, int32_t bitrate, error_s* error) {
   dcb_serial_params.fDtrControl = DTR_CONTROL_DISABLE;
 
   // set the serial port parameters to the specified values
-  if (!SetCommState(hComm, &dcb_serial_params)) {
+  if (!SetCommState(h_comm, &dcb_serial_params)) {
     *error = error_construct("setting serial port parameters failed");
-    CloseHandle(hComm);
+    CloseHandle(h_comm);
     return nullptr;
   }
 
   // specify timeouts (all values in milliseconds)
   COMMTIMEOUTS timeouts;
-  if (!GetCommTimeouts(hComm, &timeouts)) {
+  if (!GetCommTimeouts(h_comm, &timeouts)) {
     *error = error_construct("retrieving current serial port timeouts failed");
-    CloseHandle(hComm);
+    CloseHandle(h_comm);
     return nullptr;
   }
   timeouts.ReadIntervalTimeout = 50;         // max time between arrival of two bytes before ReadFile() returns
@@ -134,51 +135,41 @@ device_s device_construct(const char* port, int32_t bitrate, error_s* error) {
   timeouts.WriteTotalTimeoutMultiplier = 10; // used to calculate total time-out period for write operations
 
   // set the timeouts
-  if (!SetCommTimeouts(hComm, &timeouts)) {
+  if (!SetCommTimeouts(h_comm, &timeouts)) {
     *error = error_construct("setting serial port timeouts failed");
-    CloseHandle(hComm);
+    CloseHandle(h_comm);
     return nullptr;
   }
 
-  // create the overlapped os reader
+  // create the overlapped event handle for reading
   OVERLAPPED os_reader = {0};
-  OVERLAPPED os_write = {0};
 
   // Create the overlapped read event. Must be closed before exiting
   os_reader.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
   if (os_reader.hEvent == NULL) {
     *error = error_construct("creating overlapped read event failed");
-    CloseHandle(hComm);
-    return nullptr;
-  }
-  // Create the overlapped write event. Must be closed before exiting
-  os_write.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-  if (os_write.hEvent == NULL) {
-    *error = error_construct("creating overlapped write event failed");
-    CloseHandle(hComm);
+    CloseHandle(h_comm);
     return nullptr;
   }
 
   // set the comm mask
-  if (!SetCommMask(hComm, EV_RXCHAR | EV_ERR)) {
+  if (!SetCommMask(h_comm, EV_RXCHAR | EV_ERR)) {
     *error = error_construct("setting comm mask failed");
-    CloseHandle(hComm);
+    CloseHandle(h_comm);
     CloseHandle(os_reader.hEvent);
-    CloseHandle(os_write.hEvent);
     return nullptr;
   }
 
   // purge the comm port of any pre-existing data or errors
-  if (!PurgeComm(hComm, PURGE_RXABORT | PURGE_TXABORT | PURGE_RXCLEAR | PURGE_TXCLEAR)) {
+  if (!PurgeComm(h_comm, PURGE_RXABORT | PURGE_TXABORT | PURGE_RXCLEAR | PURGE_TXCLEAR)) {
     *error = error_construct("flushing serial port failed during serial device construction");
-    CloseHandle(hComm);
+    CloseHandle(h_comm);
     CloseHandle(os_reader.hEvent);
-    CloseHandle(os_write.hEvent);
     return nullptr;
   }
 
   // create the serial device
-  auto out = new device{hComm, os_reader, os_write};
+  auto out = new device{h_comm, os_reader, FALSE, 500};
 
   return out;
 }
@@ -190,13 +181,10 @@ void device_destruct(device_s serial) {
   device_flush(serial, &ignore);
 
   // close the serial port
-  CloseHandle(serial->fd);
+  CloseHandle(serial->h_comm);
 
   // close the overlapped read event
-  CloseHandle(serial->ro_.hEvent);
-
-  // close the overlapped write event
-  CloseHandle(serial->wo_.hEvent);
+  CloseHandle(serial->os_reader.hEvent);
 
   (void)ignore; // nothing we can do here
 
@@ -209,21 +197,68 @@ void device_read(device_s serial, void* to, int32_t len, error_s* error) {
   SWEEP_ASSERT(len >= 0);
   SWEEP_ASSERT(error);
 
-  DWORD rx_read = 0;
-  DWORD total_num_bytes_read = 0;
+  DWORD dw_num_read;
+  DWORD dw_num_to_read = (DWORD)len;
+  BOOL f_result = FALSE;
 
-  // read bytes until "len" bytes have been read
-  while (total_num_bytes_read < (DWORD)len) {
-
-    if (!ReadFile(serial->fd, (unsigned char*)to + total_num_bytes_read, len - total_num_bytes_read, &rx_read, &(serial->ro_))) {
-      *error = error_construct("reading from serial device failed");
-      return;
+  if (!serial->waiting_on_read) {
+    // Issue read operation.
+    if (!ReadFile(serial->h_comm, (unsigned char*)to, dw_num_to_read, &dw_num_read, &(serial->os_reader))) {
+      // If the read did not return immediately, check if it was pending
+      if (GetLastError() != ERROR_IO_PENDING) {
+        // Error in communications; report it.
+        *error = error_construct("reading from serial device failed");
+        f_result = FALSE;
+      } else {
+        serial->waiting_on_read = TRUE;
+      }
+    } else {
+      // read completed immediately
+      f_result = TRUE;
     }
-    total_num_bytes_read += rx_read;
   }
 
-  SWEEP_ASSERT(total_num_bytes_read == (DWORD)len && "reliable read failed to read requested number of bytes");
-  if (total_num_bytes_read != (DWORD)len)
+  DWORD dwRes;
+
+  // If the read is pending, wait for it to finish... but permit timeout
+  if (serial->waiting_on_read) {
+    dwRes = WaitForSingleObject(serial->os_reader.hEvent, serial->read_timeout_millis);
+    switch (dwRes) {
+    // Read completed.
+    case WAIT_OBJECT_0:
+      if (!GetOverlappedResult(serial->h_comm, &(serial->os_reader), &dw_num_read, FALSE)) {
+        // Error in communications; report it.
+        *error = error_construct("error in communications during serial read");
+        f_result = FALSE;
+      } else {
+        // Read completed successfully.
+        f_result = TRUE;
+      }
+
+      //  Reset flag so that another opertion can be issued.
+      serial->waiting_on_read = FALSE;
+      break;
+
+    case WAIT_TIMEOUT:
+      // Operation isn't complete yet. serial->waiting_on_read flag isn't changed since we'll loop back
+      // around, and we don't want to issue another read until the first one finishes.
+      break;
+
+    default:
+      // Error in the WaitForSingleObject; abort.
+      // Indicates a problem with the OVERLAPPED structure's event handle.
+      *error = error_construct("problem with overlapped structure during serial read");
+      f_result = FALSE;
+      break;
+    }
+  }
+
+  // Check that the number of bytes actually read is equal to the requested quantity
+  if (f_result == TRUE)
+    f_result = (dw_num_read == (DWORD)len) ? TRUE : FALSE;
+
+  SWEEP_ASSERT(f_result && "reliable read failed to read requested number of bytes");
+  if (!f_result)
     *error = error_construct("reading from serial device failed");
 }
 
@@ -234,37 +269,62 @@ void device_write(device_s serial, const void* from, int32_t len, error_s* error
   SWEEP_ASSERT(error);
 
   DWORD err = 0;
-  DWORD tx_written = 0;
-  DWORD total_num_bytes_written = 0;
+  OVERLAPPED os_write = {0};
+  DWORD dw_written;
+  DWORD dw_to_write = (DWORD)len;
+  BOOL f_result;
+
+  // Create this write's OVERLAPPED structure hEvent.
+  os_write.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+  if (os_write.hEvent == NULL) {
+    // Error creating overlapped event handle.
+    *error = error_construct("creating overlapped event handle failed during serial port write");
+    return;
+  }
 
   // check for a comm error (clears any error flag present)
-  if (!ClearCommError(serial->fd, &err, NULL)) {
+  if (!ClearCommError(serial->h_comm, &err, NULL)) {
     *error = error_construct("checking for/clearing comm error failed during serial port write");
     return;
   }
   // in the event of a comm error, purge before writing
   if (err > 0) {
-    if (!PurgeComm(serial->fd, PURGE_TXABORT | PURGE_TXCLEAR)) {
+    if (!PurgeComm(serial->h_comm, PURGE_TXABORT | PURGE_TXCLEAR)) {
       *error = error_construct("purging tx buffer failed during serial port write");
       return;
     }
   }
 
-  // write bytes until "len" bytes have been written
-  while (tx_written < (DWORD)len) {
-    if (!WriteFile(serial->fd,                                           // Handle to the Serial port
-                   (const unsigned char*)from + total_num_bytes_written, // Data to be written to the port
-                   len - total_num_bytes_written,                        // No of bytes to write
-                   &tx_written,                                          // Bytes written
-                   &(serial->wo_))) {
-      *error = error_construct("writing to serial device failed");
-      return;
+  // Issue write.
+  if (!WriteFile(serial->h_comm, (const unsigned char*)from, dw_to_write, &dw_written, &os_write)) {
+    if (GetLastError() != ERROR_IO_PENDING) {
+      // WriteFile failed, but it isn't delayed. Report error and abort.
+      *error = error_construct("WriteFile failed, but is not delayed during serial port write");
+      f_result = FALSE;
+    } else {
+      // Write is pending indefinitely (ie: fWait param is TRUE, so this is effectively NON-OVERLAPPED io)
+      if (!GetOverlappedResult(serial->h_comm, &os_write, &dw_written, TRUE)) {
+        *error = error_construct("Failed to get result during serial port write");
+        f_result = FALSE;
+      } else {
+        // Write operation completed successfully.
+        f_result = TRUE;
+      }
     }
-    total_num_bytes_written += tx_written;
+  } else {
+    // WriteFile completed immediately.
+    f_result = TRUE;
   }
 
-  SWEEP_ASSERT((total_num_bytes_written == (DWORD)len) && "reliable write failed to write requested number of bytes");
-  if (total_num_bytes_written != (DWORD)len)
+  // Check that the number of bytes actually transferred is equal to the requested quantity
+  if (dw_written != (DWORD)len)
+    f_result = FALSE;
+
+  // Close the overlapped event handle
+  CloseHandle(os_write.hEvent);
+
+  SWEEP_ASSERT(f_result && "reliable write failed to write requested number of bytes");
+  if (f_result == FALSE)
     *error = error_construct("writing to serial device failed");
 }
 
@@ -274,7 +334,7 @@ void device_flush(device_s serial, error_s* error) {
 
   DWORD err = 0;
   // check for a comm error (clears any error flag present)
-  if (!ClearCommError(serial->fd, &err, NULL)) {
+  if (!ClearCommError(serial->h_comm, &err, NULL)) {
     *error = error_construct("checking for/clearing comm error failed during serial port flush");
     return;
   }
@@ -282,7 +342,7 @@ void device_flush(device_s serial, error_s* error) {
   // flush serial port
   // - Empty Tx and Rx buffers
   // - Abort any pending read/write operations
-  if (!PurgeComm(serial->fd, PURGE_RXABORT | PURGE_TXABORT | PURGE_RXCLEAR | PURGE_TXCLEAR)) {
+  if (!PurgeComm(serial->h_comm, PURGE_RXABORT | PURGE_TXABORT | PURGE_RXCLEAR | PURGE_TXCLEAR)) {
     *error = error_construct("flushing serial port failed");
   }
 }
